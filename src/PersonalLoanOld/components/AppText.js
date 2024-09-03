5bb95626726d5df214d5641947cf3612e506c57c
aditya.knight <aditya.sharma@knightfintech.com>
2024-09-03 23:33:41 +0530

    removed dependency

--- src/PersonalLoanOld/components/AppText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text, TextProps } from 'react-native';
-
-const AppText = ({ style, ...props }) => {
-  return <Text style={[{ fontFamily: 'Poppins_400Regular' }, style]} {...props} />;
-};
-
-export default AppText;
